e75de181215604bab5c5670b0eaed2c5b80f73bc
jaypeequijano <35474664+jaypeequijano@users.noreply.github.com>
2019-01-23 14:32:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <title>Use Case Diagram</title>
 </head>
 <body>
-<img src="usecase.jpg">
+<img src="usecase1.jpg">
 <p>This diagram shows the process of both actors involve in these proposed app.There are two types of user.The tutor and client, tutor provides the service and client are the one being served. User are required to select what type of user they are between being a tutor and a client. For tutors they can update profile. The are the one that chooses subjects they specialized but depends of the availability of particular subject. They can view his/her clients and able to confirm or cancel an appoinment. They can also view the ratings made by there past client. The higher the rate the bigger the chance of opportunities. They're are in control of there availability, it's up to them about activity schedules.In clients side. They're the most important priority here because they are the ones who pay for the tutors.All they had to do is find what particular subject they need,find available tutor within their location make an appoinment and pay for it.The clients has the power to rate a particular tutor. This remarks serves as a recommendation for other client and how competence and capable the tutor might be.Upon checking the schedule of the tutors will decide if that tutor can provide the service that you need or not.Lastly the Admin, the one who is responsible of everything. In control of both user, the client and the tutor. Can update the profile of users. Block and delete user who are inactive for long time and who are irresponsible.By this means it can protect both the client and the tutors. The only one who has access to information of both parties.And lastly the one who confirms the cancellation of appointments. </p>
 </body>
 </html>
